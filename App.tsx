0c4d15ecbc2db780c5b96bf1a7061adf095628b3
unaicanales <36845145+Seretoiden@users.noreply.github.com>
2022-11-25 19:07:06 +0100

    Onboarding Name Activity
    Added

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { useCallback } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Keyboard, StyleSheet, Text, TextInput, View } from "react-native";
 import { MMKV } from "react-native-mmkv";
+
+import Header from "./components/header/Header";
+import TextOnboarding from "./components/onboarding/TextOnboarding";
 
 export const storage = new MMKV({
   id: `user-storage`,
@@ -32,21 +35,71 @@ export default function App() {
     });
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button onPress={handleSave} title="save" />
-      <Button onPress={handleRead} title="Read now" />
-    </View>
+  const text = "Nombre..."
+
+  return (  
+      <View style={styles.container}>
+        <View style={styles.upside_container}>
+          <Header></Header>
+          <TextOnboarding text="Bienvenido,
+          Esta aplicación te servirá para adquirir consciencia sobre tus emociones en tu día a día. Con ella podrás tener un calendario con el histórico de tu estado emocional, podrás expresar como te sientes, decir lo que has hecho en el día y ver diferentes gráficas para analizar tus estados de animo.  "/>
+          <Text style={{margin:20}}>Para una experiencia personalizada, puedes introducir tu nombre:</Text>
+          <TextInput
+            style={styles.input}
+            value={text}
+          />
+        </View>
+        <View style={styles.bottom_container}>
+          
+          <View onTouchEnd={handleRead}>
+              <Text>SALTAR</Text>
+            </View>
+
+          <View style={styles.verticleLine}></View>
+
+          <View onTouchEnd={handleRead}>
+            <Text>SIGUIENTE</Text>
+          </View>
+            
+        </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    margin: 30,
+
   },
+  upside_container: {
+    flex: 1,
+    fontSize: 16,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+
+  bottom_container: {
+    padding:10,
+    paddingTop: 70,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+  },
+  
+  input: {
+    borderColor: "gray",
+    borderWidth: 0.8,
+    margin:20,
+    borderRadius: 10,
+    padding: 10,
+  },
+  verticleLine: {
+    height: '100%',
+    width: 1,
+    backgroundColor: '#909090',
+  }
 });
