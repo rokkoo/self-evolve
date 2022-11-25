63daf70e64ced507ca3de1be6231f9a6ba88592a
alfonso <alfonsolasaguirre@gmail.com>
2022-11-25 16:15:31 +0100

    set up eas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { MMKV } from "react-native-mmkv";
+
+export const storage = new MMKV({
+  id: `user-storage`,
+  encryptionKey: "hunter2",
+});
 
 export default function App() {
+  const handleSave = useCallback(() => {
+    storage.set("user.name", "Marc");
+    storage.set("user.age", 21);
+    storage.set("is-mmkv-fast-asf", true);
+  }, []);
+
+  const handleRead = useCallback(() => {
+    const keys = storage.getAllKeys();
+    console.log({ keys });
+
+    const username = storage.getString("user.name"); // 'Marc'
+    const age = storage.getNumber("user.age"); // 21
+    const isMmkvFastAsf = storage.getBoolean("is-mmkv-fast-asf"); // true
+    const nn = storage.getBoolean("is-asf"); // true
+
+    console.log({
+      username,
+      age,
+      isMmkvFastAsf,
+      nn,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={handleSave} title="save" />
+      <Button onPress={handleRead} title="Read now" />
     </View>
   );
 }
@@ -13,8 +45,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
