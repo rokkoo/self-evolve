5593ada2849e0aa5740d10c8998390495c78f620
alfonso <alfonsolasaguirre@gmail.com>
2022-11-26 15:28:09 +0100

    core: add gesture handler

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigationContainer from "./src/navigation";
 
 export default function App() {
-  return <AppNavigationContainer />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppNavigationContainer />
+    </GestureHandlerRootView>
+  );
 }
