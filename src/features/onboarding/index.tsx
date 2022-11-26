baa2efd6ade7ae5b0cb93ef3933cafb8137c417d
alfonso <alfonsolasaguirre@gmail.com>
2022-11-26 14:21:38 +0100

    fix: onboarding margins

--- src/features/onboarding/index.tsx
+++ src/features/onboarding/index.tsx
@@ -5,35 +5,37 @@ import useOnboarding from "./hooks/useOnboarding";
 import AppLayoutScrollView from "../../component/AppLayoutScrollView";
 import BottomButtons from "./components/bottomButtons";
 import TextOnboarding from "./components/TextOnboarding";
+import AppLayout from "../../component/AppLayout";
 
 const Onboarding = () => {
   const { handleInputChange, username } = useOnboarding();
 
-  // FIX MARGINS
   return (
-    <AppLayoutScrollView style={{ flex: 1 }}>
-      <View style={styles.upsideContainer}>
-        <Header />
-        <TextOnboarding
-          text="Bienvenido,
+    <AppLayout>
+      <AppLayoutScrollView style={{ flex: 1 }}>
+        <View style={styles.upsideContainer}>
+          <Header />
+          <TextOnboarding
+            text="Bienvenido,
           Esta aplicación te servirá para adquirir consciencia sobre tus emociones en tu día a día. Con ella podrás tener un calendario con el histórico de tu estado emocional, podrás expresar como te sientes, decir lo que has hecho en el día y ver diferentes gráficas para analizar tus estados de animo.  "
-        />
-        <Text style={{ margin: 20 }}>
-          Para una experiencia personalizada, puedes introducir tu nombre:
-        </Text>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.input}
-            value={username}
-            onChangeText={handleInputChange}
-            placeholder="Name"
-            maxLength={12}
-            scrollEnabled={false}
           />
+          <Text style={{ margin: 20 }}>
+            Para una experiencia personalizada, puedes introducir tu nombre:
+          </Text>
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              value={username}
+              onChangeText={handleInputChange}
+              placeholder="Name"
+              maxLength={12}
+              scrollEnabled={false}
+            />
+          </View>
         </View>
-      </View>
-      <BottomButtons username={username}></BottomButtons>
-    </AppLayoutScrollView>
+        <BottomButtons username={username}></BottomButtons>
+      </AppLayoutScrollView>
+    </AppLayout>
   );
 };
 
