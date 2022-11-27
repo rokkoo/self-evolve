268944233015d2c748e6ca7c3fa5d887e5e8d3d7
alfonso <alfonsolasaguirre@gmail.com>
2022-11-27 10:12:54 +0100

    feat: create header container

---
+++ src/features/home/components/header/index.tsx
@@ -0,0 +1,38 @@
+import { useCallback } from "react";
+import { View, Image, TouchableOpacity, StyleSheet } from "react-native";
+import { FlowEnum } from "../../../../navigation/types";
+import { LayoutMetrics } from "../../../../constants/metrics";
+import useAppNavigation from "../../../../navigation/hooks/useAppNavigation";
+
+const Header = () => {
+  const { navigation } = useAppNavigation();
+
+  const handleSettingsPress = useCallback(() => {
+    navigation.navigate(FlowEnum.DailyResumePost);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={handleSettingsPress}>
+        <Image
+          source={require("../../../../../assets/icons/settings.png")}
+          style={styles.settingsImage}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingVertical: 12,
+    paddingHorizontal: LayoutMetrics.horizontalSpacing,
+    alignItems: "flex-end",
+  },
+  settingsImage: {
+    width: 28,
+    height: 28,
+  },
+});
+
+export default Header;
