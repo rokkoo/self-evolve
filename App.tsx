f9797e0ad355d860ae2d3d44313cc63b2eb2130f
alfonso <alfonsolasaguirre@gmail.com>
2022-11-27 12:05:04 +0100

    feat: import random values on app intry point

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigationContainer from "./src/navigation";
 
+// Necesary to use uuid
+import "react-native-get-random-values";
+
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
