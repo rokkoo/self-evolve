96f7c33877c8f3d930efac1f205fb76591c4202a
alfonso <alfonsolasaguirre@gmail.com>
2022-11-27 14:02:12 +0100

    code: add toas package

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import Toast from "react-native-toast-message";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigationContainer from "./src/navigation";
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <AppNavigationContainer />
+      <Toast />
     </GestureHandlerRootView>
   );
 }
