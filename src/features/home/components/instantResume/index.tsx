281273a5ea60f080c9902a600f37c5f09c8fd4cb
alfonso <alfonsolasaguirre@gmail.com>
2022-11-27 11:11:06 +0100

    feat; export necesary types

--- src/features/home/components/instantResume/index.tsx
+++ src/features/home/components/instantResume/index.tsx
@@ -4,37 +4,37 @@ import { TagType, TagsEnum } from "../tagsContainer/types";
 import { styles } from "./styles";
 
 export enum EmotionalStatusEnum {
-    HAPPY = 'happy',
-    SAD = 'sad',
+  HAPPY = "happy",
+  SAD = "sad",
 }
 
-type EmotionStatus = typeof EmotionalStatusEnum[keyof typeof EmotionalStatusEnum]
+export type EmotionStatus =
+  typeof EmotionalStatusEnum[keyof typeof EmotionalStatusEnum];
 
 interface Props {
-    text: string,
-    createdAt: string,
-    emotionalStatus: EmotionStatus,
-    tags: TagType[]
+  text: string;
+  createdAt: string;
+  emotionalStatus: EmotionStatus;
+  tags: TagType[];
 }
 
-const InstantResume:React.FC<Props> = (props) => {
-    return (
-        <View style={styles.container}>
-            <View style={styles.row}>
-                <View style={styles.left}>
-                    <Text style={styles.hour}>{props.createdAt}</Text>
-                    <Text style={styles.text}>{props.text}</Text>
-                </View>
-                <View style={styles.right}>
-                    <View style={styles.emotionalStatus}></View>
-                </View>
-                
-            </View>
-            <View style={styles.tagsbox}>
-                <TagsContainer tags={props.tags}/>
-            </View>
+const InstantResume: React.FC<Props> = (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.row}>
+        <View style={styles.left}>
+          <Text style={styles.hour}>{props.createdAt}</Text>
+          <Text style={styles.text}>{props.text}</Text>
         </View>
-    )
-}
+        <View style={styles.right}>
+          <View style={styles.emotionalStatus}></View>
+        </View>
+      </View>
+      <View style={styles.tagsbox}>
+        <TagsContainer tags={props.tags} />
+      </View>
+    </View>
+  );
+};
 
 export default InstantResume;
